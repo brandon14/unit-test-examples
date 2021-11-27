9c5ea87901a1afb09daf42849e03448bc658161e
brandon14 <brandon14@users.noreply.github.com>
2021-11-27 20:24:05 +0000

    Fix styling

--- doctum.php
+++ doctum.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ * This file is part of the brandon14/unit-test-examples package.
+ *
+ * Copyright (c) 2018-2020 Brandon Clothier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+declare(strict_types=1);
+
 use Doctum\Doctum;
 use Symfony\Component\Finder\Finder;
 use Doctum\RemoteRepository\GitHubRemoteRepository;
@@ -12,7 +33,7 @@ $iterator = Finder::create()
 
 return new Doctum($iterator, [
     'title'             => 'brandon14/unit-test-examples Documentation',
-    'source_dir'        => dirname($dir) . '/',
+    'source_dir'        => dirname($dir).'/',
     'remote_repository' => new GitHubRemoteRepository('brandon14/unit-test-examples', dirname($dir)),
     'build_dir'         => __DIR__.'/docs',
     'cache_dir'         => __DIR__.'/doctum_cache',
